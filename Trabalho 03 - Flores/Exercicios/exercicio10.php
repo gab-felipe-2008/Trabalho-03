2769e00f9992d09e544f9643fa4cebc285a91ae6
Victor-Asf <asafantunes09@gmail.com>
2026-09-16 15:48:29 -0300

    Update exercicio10.php

--- Trabalho 03 - Flores/Exercicios/exercicio10.php
+++ Trabalho 03 - Flores/Exercicios/exercicio10.php
@@ -13,4 +13,11 @@ Componentes:
 Data: 19 de Setembro de 2026
 Descritivo: Escreva um programa que leia dois numeros inteiros, calcule a soma deles e exiba o resultado precedido pela palavra 'Soma: '.
 *******************************************************************************/
+$numero1 = (int) readline("Digite o primeiro número: ");
+$numero2 = (int) readline("Digite o segundo número: ");
 
+$soma = $numero1 + $numero2;
+
+echo "Soma: $soma";
+
+?>
