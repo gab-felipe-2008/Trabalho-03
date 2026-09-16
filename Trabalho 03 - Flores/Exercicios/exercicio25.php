5263240225cc831f32fbd355b1604f0a844f4c54
Marcio Jose Aguiar Filho <marciojoseaguiarf@gmail.com>
2026-09-15 22:20:44 -0300

    Update exercicio25.php

--- Trabalho 03 - Flores/Exercicios/exercicio25.php
+++ Trabalho 03 - Flores/Exercicios/exercicio25.php
@@ -13,4 +13,9 @@ Componentes:
 Data: 19 de Setembro de 2026
 Descritivo: Escreva um programa que leia uma temperatura dada na escala Fahrenheit e exiba o equivalente convertido em graus Celsius (Formula de conversao: C = 5/9 * (F - 32)).
 *******************************************************************************/
+$temFah = readline("Digite a temperatura em Fahrenheit: "); //Usuário digita a temperatura em Fahrenheit para converter em graus celsius.
+$temCel = 5/9 * ($temFah - 32) ; // Fórmula para conversão em graus Celsius
+$temCel = round($temCel,2); // Deixar o resultado da temperatura °C com apenas 2 casas decimais
+echo "A temperatura em Fahrenheit convertida para graus Celsius é: ", $temCel,"°C";
 
+?>
