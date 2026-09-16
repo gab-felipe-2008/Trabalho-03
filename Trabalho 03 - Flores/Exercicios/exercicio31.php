4db037d63f3ff1109257d90c7a880617885f485c
Marcio Jose Aguiar Filho <marciojoseaguiarf@gmail.com>
2026-09-15 22:40:12 -0300

    Update exercicio31.php

--- Trabalho 03 - Flores/Exercicios/exercicio31.php
+++ Trabalho 03 - Flores/Exercicios/exercicio31.php
@@ -11,6 +11,17 @@ Componentes:
               260081732 - Alan Cristian dos Santos Costa
               260103602 - Victor Asaf Antunes dos Santos
 Data: 19 de Setembro de 2026
-Descritivo: Escreva um programa que leia um numero inteiro e exiba se ele � PAR ou IMPAR.
+Descritivo: Escreva um programa que leia um numero inteiro e exiba se ele é PAR ou IMPAR.
 *******************************************************************************/
+    //Leitura do n° inteiro
+    $numInt = readline ("Escreva um número inteiro: ");
 
+    //Estrutura de condição para ver se tal n° é par ou ímpar
+    if ($numInt % 2 === 0){ 
+        /* Se o resto da divisão desse número por 2 for 0 então ele é par */
+        echo "O número ",$numInt, " é par.";    
+    }else{
+        echo "O número ",$numInt, " é impar.";
+    }
+
+?>
