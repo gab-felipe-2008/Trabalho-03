9e038562a658f366440c64ee7e1e012927c7762e
Marcio Jose Aguiar Filho <marciojoseaguiarf@gmail.com>
2026-09-15 23:40:47 -0300

    Update exercicio34.php

--- Trabalho 03 - Flores/Exercicios/exercicio34.php
+++ Trabalho 03 - Flores/Exercicios/exercicio34.php
@@ -13,4 +13,14 @@ Componentes:
 Data: 19 de Setembro de 2026
 Descritivo: Escreva um programa que leia dois numeros reais e exiba o maior de forma clara (abordagem focada em logica estruturada de decisao composta).
 *******************************************************************************/
+    //LEITURA DE 2 NÚMEROS REAIS
+    $n1 = readline("Digite um número: ");
+    $n2 = readline("Digite outro número: ");
 
+    //Estrutura de decisão composta
+    if ($n1 > $n2){ // Mostra que o maior número digitado foi o 1°.
+        echo "O maior número que você digitou foi o ", $n1, ".",PHP_EOL, "O outro número digitado foi ", $n2,".";
+    }else{ // Mostra que o maior número digitado foi o 2°.
+        echo "O maior número que você digitou foi o ", $n2, ".",PHP_EOL, "O outro número digitado foi ", $n1,".";
+    }
+?>
