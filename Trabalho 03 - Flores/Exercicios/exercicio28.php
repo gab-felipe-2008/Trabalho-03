2863f9d6ddcef519c7a1b1f3e1315452ef8a46fa
Marcio Jose Aguiar Filho <marciojoseaguiarf@gmail.com>
2026-09-15 22:22:10 -0300

    Update exercicio28.php

--- Trabalho 03 - Flores/Exercicios/exercicio28.php
+++ Trabalho 03 - Flores/Exercicios/exercicio28.php
@@ -13,4 +13,15 @@ Componentes:
 Data: 19 de Setembro de 2026
 Descritivo: Escreva um programa que leia um numero real e, se ele for maior do que 20, exiba a metade deste numero.
 *******************************************************************************/
+//Leitura do n° real
+$numRea = readline("Digite aqui um número qualquer: ");
 
+//Estrutura de condição para saber se o n° é maior ou menor que 20 e calcular sua metade
+if ($numRea > 20){
+    $met = $numRea / 2; //Calculo da metade do n° real caso for maior que 20.
+    $met = round($met,2); //Deixei o valor da metade apenas com 2 casas decimais se houver vírgula.
+    echo "O número que você informou é maior que 20. O valor da sua metade é: ", $met, ".";
+}else{
+    echo "O número informado é menor que 20, não será possível calcular sua metade.";
+}
+?>
