ecc121a28870af17ce5aa234dd08c109e828f7a8
Marcio Jose Aguiar Filho <marciojoseaguiarf@gmail.com>
2026-09-16 20:15:55 -0300

    Update exercicio36.php

--- Trabalho 03 - Flores/Exercicios/exercicio36.php
+++ Trabalho 03 - Flores/Exercicios/exercicio36.php
@@ -14,10 +14,10 @@ Data: 19 de Setembro de 2026
 Descritivo: Deseja-se calcular a conta de consumo de energia eletrica de um consumidor. Escreva um programa que leia o codigo do consumidor, o preco do kWh e a quantidade de kWh consumida. Exiba o codigo do consumidor e o total a pagar, sabendo que a taxa minima cobrada é de R$ 11,20.
 *******************************************************************************/
     //LER CÓDIGO DO CONSUMIDOR
-    $codCon = readline("Digite seu código de consumidor: ");
+    $codCon = (int) readline("Digite seu código de consumidor: ");
     //LER PREÇO DO kWh
-    $preKwh = readline("Digite o preço do kWh: ");
-    $preKwh = round($preKwh,2);//Deixa a variável $preKwh com 2 casas decimais
+    $preKwh = (float) readline("Digite o preço do kWh: ");
+    $preKwh =round($preKwh,2);//Deixa a variável $preKwh com 2 casas decimais
     //LER QUANTIDADE DE KWH CONSUMIDA
     $qtdCon = readline("Digite a quantidade de kWh consumida: ");
     //CÁLCULO DE QUANTO O CONSUMIDOR VAI TER QUE PAGAR
@@ -33,4 +33,7 @@ Descritivo: Deseja-se calcular a conta de consumo de energia eletrica de um cons
     }else{ //MOSTRA A QUANTIA A SER PAGA SE O VALOR SUPERAR A TARIFA MÍNIMA
         echo "O valor total a ser pago é de: R$",$pagar;
     }
+
+/*PROGRAMA LÊ O CPODIGO DO CONSUMIDOR, PREÇO DO KWH E A QUANTIDADE DE ENERGIA CONSUMIDA. APÓS AS INFORMAÇÕES RECEBIDAS ELE VAI EXIBIR O CÓDIGO DE CONSUMIDOR E O TOTAL 
+A PAGAR. PROGRAMA VAI TAMBÉM IDENTIFICAR SE O CONSUMIDOR NÃO UTRAPASSOU A TARIFA MÍNIMA, CASO NÃO TENHA ULTRAPASSADO O PROGRAMA VAI COBRAR SOMENTE 11,,20.*/
 ?>
